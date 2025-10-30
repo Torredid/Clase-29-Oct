d6e38f74d612756fa779d7ad2e1d49c4dffe44dd
Torredid <dagduggtdiego@gmail.com>
2025-10-29 19:42:17 -0600

    Cambios de main y en las clases

--- HiloEscribe.java
+++ HiloEscribe.java
@@ -3,18 +3,40 @@ public class HiloEscribe extends Thread{
     private int ini;
     private int tmp;
     private Posicion p;
-
+    private boolean terminarHilo = false;
+    private boolean pausarHilo = false;
+    
+    
     public HiloEscribe(Posicion x){
         this.p=x;
     }
-    
     public void setTmp(int tmp) {
         this.tmp = tmp;
     }
+    public void setTerminar(boolean t) {
+        this.terminarHilo = t;
+    }
+    public synchronized void setPausar(boolean p) {
+        this.pausarHilo = p;
+        if (!p) {
+            notify(); 
+        }
+    }
     @Override
     public void run(){
-        for(int i=ini;i<ini+5;i++)
+        for(int i=ini; i < ini + 5 && !terminarHilo; i++)
         {
+            try {
+                synchronized (this) {
+                    while (pausarHilo) { 
+                        wait();
+                    }
+                }
+            } catch (InterruptedException e) {
+                if(terminarHilo) {
+                    return;
+                }
+            }
            try{
                 synchronized(p){
                     info[p.getP()]=getName()+i+"-"+p.getP();
@@ -22,22 +44,23 @@ public class HiloEscribe extends Thread{
                 }
                 sleep(tmp);
            }
-            catch(ArrayIndexOutOfBoundsException e){
+           catch(ArrayIndexOutOfBoundsException e){
                 System.out.println("NO HAY ESPACIO");
-                i=ini+5;
+                break;
             }
            catch(InterruptedException e){
-                System.out.println("INTERRUPCIÓN");
+                System.out.println("INTERRUPCION");
+                if(terminarHilo) {
+                    break;
+                }
             }
         }
     }
-    
     public void setIni(int x){
         ini=x;
     }
-    
     public void setInfo(String x[]){
         info=x;
     }
-    
 }
+ 
--- Posicion.java
+++ Posicion.java
@@ -1,13 +1,12 @@
 public class Posicion {
     private int p=0;
-
     public int getP() {
-        return p;
+    return p;
     }
-
     public void incrementa() {
-        this.p++;
+    this.p++;
     }
-    
-    
+    public void setP(int nuevoP) {
+    this.p = nuevoP;
+    }
 }
